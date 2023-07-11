0a149b2fa87386b712a8de0cc883f33546a04bca
WaqyWise <vakulenko.a@ukr.net>
2023-07-11 16:22:01 +0300

    CartBootStrap

--- src/views/Cart.js
+++ src/views/Cart.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { updateQuantity, removeItem } from '../store/modules/cart'
 import product from '../store/modules/product';
-import { Image } from 'react-bootstrap';
+import { Image, Card, Col, Row, ListGroup, Button, InputGroup,Form } from 'react-bootstrap';
 
 
 
@@ -21,28 +21,38 @@ const Cart = () => {
   
     return (
         <div>
-        <h1>Cart :</h1>
+        <h1 style={{justifyContent: 'center'}}>Cart :</h1>
         {cartItems.length === 0 ? (
           <p>Your cart is empty.</p>
         ) : (
-          <ul>
+          <Row className="px-2 my-5 " style={{alignItems :'center' , justifyContent: 'center'}}>
             {cartItems.map((item) => (
-              <li key={item.product.id}>
-                {/* <Image>{item.product.image}</Image> */}
-                <h3>{product.name}</h3>
-                <p>Price: {item.product.price}</p>
-                <p>
-                  Quantity:
-                  <input
-                    type="number"
-                    value={item.quantity}
-                    onChange={(e) => handleQuantityChange(item.product.id, parseInt(e.target.value))}
-                  />
-                </p>
-                <button onClick={() => handleRemoveItem(item.product.id)}>Remove</button>
-              </li>
+              <Col key={item.product.id} sm={7}>
+                 <Col className='text-center my-5 '>
+                    <Image align="center" src={item.product.image} fluid width={80} height={80} />
+                </Col>
+                <ListGroup class="outline" className="list-group-flush">
+                <h2>{item.product.title}</h2>
+                <h2>Product Description :</h2>
+                <ListGroup.Item>Price: ${item.product.price}</ListGroup.Item>
+                <ListGroup.Item>Rating: {item.product.rating.rate}</ListGroup.Item>
+                <ListGroup.Item>Reviews: {item.product.rating.count}</ListGroup.Item>
+
+                </ListGroup>
+               
+                <InputGroup>
+                <h2>Quantity:</h2>
+                <Form.Control
+                placeholder="Recipient's username"
+                type="number"
+                value={item.quantity}
+                onChange={(e) => handleQuantityChange(item.product.id, parseInt(e.target.value))}
+                />
+            </InputGroup>
+                <Button onClick={() => handleRemoveItem(item.product.id)} variant="danger" >Remove</Button>
+              </Col>
             ))}
-          </ul>
+          </Row>
         )}
       </div>
 
